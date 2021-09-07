93f3a8e2345da99587ee0f985a55444d69e10787
RupeshYadav4 <86247908+rupesh0097@users.noreply.github.com>
2021-09-07 07:24:15 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    //C++ Maths.
+    //c++ has many functions that perform mathematical operations in numbers.
+    cout << max(23,45) << endl;
+    cout << min(99,100) << endl;
+    cout << sqrt(64) << endl;
+    cout << round(2.4) << endl;
+    cout << round(2.6) << endl;
+    cout << log(2) << endl;
+    cout << abs(-5);
+    cout << "  The absolute value of -5 is 5." << endl;
+    cout << cbrt(8);
+    cout << "  The cuberoot of 8 is 2." << endl;
+    cout << fdim(20,30);
+    cout << "  Returns the positive difference between x and y " << endl;
+    cout << pow(2,3);
+    cout << "  Returns the value of x to the power of y" << endl;
+   
+    return 0;
+    
+}
